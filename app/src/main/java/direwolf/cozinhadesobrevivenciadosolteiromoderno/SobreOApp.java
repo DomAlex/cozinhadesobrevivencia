36372437b37f4aee7c6c70a97214882fd9e49bef
Direwolf <dom-alex-araujo@outlook.com>
2015-08-11 02:33:18 -0400

    processo de implementação do navigation drawer na tela sobre.

--- app/src/main/java/direwolf/cozinhadesobrevivenciadosolteiromoderno/SobreOApp.java
+++ app/src/main/java/direwolf/cozinhadesobrevivenciadosolteiromoderno/SobreOApp.java
@@ -1,16 +1,52 @@
 package direwolf.cozinhadesobrevivenciadosolteiromoderno;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class SobreOApp extends AppCompatActivity {
+public class SobreOApp extends AppCompatActivity{
+
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private DrawerLayout drawerLayout;
+    private ArrayAdapter<String> adapter;
+    private ListView drawerList;
+    private String tituloDaActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre_oapp);
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        drawerList = (ListView)findViewById(R.id.navListActivitySobre);
+        tituloDaActivity = getTitle().toString();
+
+
+
+    }
+
+
+    protected void addDrawerItems(){
+        String[] itemsDoNavigationDrawer = {"Inicio","Receitas Salgadas","Receitas Doces","Aperitivos","Molhos","Sobre o app"};
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,itemsDoNavigationDrawer);
+        drawerList.setAdapter(adapter);
+        drawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Xablau!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    protected  void aetUpDrawer(){
 
 
     }
